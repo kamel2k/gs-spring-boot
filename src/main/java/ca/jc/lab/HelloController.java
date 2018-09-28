61882f58b24fe323daf5ea437c67454d40922c84
vagrant <vagrant@localhost.localdomain>
2018-09-28 16:28:26 +0000

    change package

--- src/main/java/ca/jc/lab/HelloController.java
+++ src/main/java/ca/jc/lab/HelloController.java
@@ -1,4 +1,4 @@
-package hello;
+package ca.jc.lab;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/ca/jc/lab/HelloControllerIT.java
+++ src/test/java/ca/jc/lab/HelloControllerIT.java
@@ -1,4 +1,4 @@
-package hello;
+package ca.jc.lab;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
